5a9f5316363bd6a55829f2b527576500a36dc9e1
Maxwell <Maxwellbarboza@hotmail.com>
2023-04-08 16:16:35 -0300

    Add endpoint de atualizar e cadastrar

--- controladores/autores.js
+++ controladores/autores.js
@@ -1,22 +1,91 @@
 const conexao = require('../conexaoDB')
 
 const listarAutores = async (req, res) => {
-    const livros = await conexao.query('select * from livros');
 
-    res.status(201).json(livros);
+    try{
+        const { rows: autores } = await conexao.query('select * from autores');
 
+        if (autores.rowCount === 0){
+            return res.status(404).json('Não existe registro nesta tabela.');
+        }
+        return res.status(200).json(autores);
+
+    }catch (error){
+        return res.status(400).json(error.message);
+
+    }
 }
 
 const obterAutor = async (req, res) => {
+    const { id } = req.params;
+    try{
+        const query = 'select * from autores where id = $1'
+        const autor = await conexao.query(query, [id]);
+        
+        if (autor.rowCount === 0){
+            return res.status(404).json('Autor não encontrado.');
+
+        }
+        
+        return res.status(200).json(autor.rows[0]);
+
+    }catch (error){
+        return res.status(400).json(error.message);
+
+    }
+
+}
+
+const cadastrarAutor = async (req, res) => {
+    const {nome, idade} = req.body;
+    if (!nome){
+        return res.status(400).json("O campo nome é obrigatório.");
+    }
+
+    try{
+        const query = 'insert into autores (nome, idade) values ($1, $2)'
+        const autor = await conexao.query(query , [nome, idade]);
+
+        if (autor.rowCount = 0){
+            return res.status(400).json('Não foi possivel cadastrar o autor.');
+        }
+        return res.status(200).json('Autor cadastrado com sucesso.');
+
+    }catch (error){
+        return res.status(400).json(error.message);
+    }
 
 }
 
 const atualizarAutor = async (req, res) => {
+    const { id } = req.params;
+    const {nome, idade} = req.body;
+    try{
+        const autor = await conexao.query('select * from autores where id = $1', [id]);
+        
+        if (autor.rowCount === 0){
+            return res.status(404).json('Autor não encontrado.');
+        }
 
-}
-const cadastrarAutor = async (req, res) => {
+        if (!nome){
+            return res.status(400).json("O campo nome é obrigatório.");
+        }
 
+        const query = 'update autores set nome = $1, idade = $2 where id = $3'
+        const autorAtualizado = await conexao.query(query, [nome, idade, id]);
+
+        if (autor.rowCount === 0){
+            return res.status(404).json('Não foi possível atualizar o autor.');
+        }
+        
+        return res.status(200).json('Autor foi atualizado com sucesso.');
+
+    }catch (error){
+        return res.status(400).json(error.message);
+
+    }
 }
+
 const deletarAutor = async (req, res) => {
 
 }
